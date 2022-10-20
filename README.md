d617a17496dedc5ec2429bba6698b042b7c44ce3
rigood <rigood@naver.com>
2022-10-20 21:56:53 +0900

    🎉 Set up

---
+++ README.md
@@ -0,0 +1,10 @@
+<bn>
+
+# 리액트 공부
+
+#### 2022. 10. 20. 목요일
+
+- [search-filter](practice/src/search-filter/README.md)
+- [react-hook-mistakes](study/react-hook-mistakes.md)
+- [pokemon-app](practice/src/pokemon-list/README.md)
+- [book-search](practice/src/book-search/README.md)
---
+++ practice/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import SearchFilter from "./search-filter/SearchFilter";
+// import PokemonList from "./pokemon-list/PokemonList";
+// import BookSearch from "./book-search/BookSearch";
+
+export default function App() {
+  return (
+    <>
+      {/* <SearchFilter /> */}
+      {/* <PokemonList /> */}
+      {/* <BookSearch /> */}
+    </>
+  );
+}
